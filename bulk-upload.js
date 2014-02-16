d8aab8a1697c862cc8e5aab14b080d3c2c561a74
John Resig <jeresig@gmail.com>
2014-02-16 13:15:17 -0500

    Tools for interacting with MatchEngine.

---
+++ bulk-upload.js
@@ -0,0 +1,89 @@
+var fs = require("fs");
+var path = require("path");
+var async = require("async");
+var ArgumentParser = require("argparse").ArgumentParser;
+
+var argparser = new ArgumentParser({
+    description: "Upload a directory of images to MatchEngine."
+});
+
+argparser.addArgument(["dir"], {
+    help: "The directory in which to place the images in MatchEngine " +
+        "(e.g. 'camping')."
+});
+
+argparser.addArgument(["path"], {
+    help: "The directory path of images to upload (e.g. /my/images/camping/)."
+});
+
+argparser.addArgument(["--conf"], {
+    defaultValue: __dirname + "/me.conf.json",
+    help: "The JSON config file containing MatchEngine auth details " +
+        "(default: me.conf.json)."
+});
+
+argparser.addArgument(["--batch"], {
+    defaultValue: 1000,
+    type: "int",
+    help: "The number of images to upload in a single batch (up to 1000) " +
+        "(default: 1000)."
+});
+
+argparser.addArgument(["--pause"], {
+    defaultValue: 0,
+    type: "int",
+    help: "How long to pause, in milliseconds, inbetween uploads (default: 0)."
+});
+
+var args = argparser.parseArgs();
+
+var conf = require(args.conf);
+var ME = require("matchengine")(conf);
+
+console.log("Downloading file data from MatchEngine...");
+
+ME.list(function(meData) {
+    fs.readdir(args.path, function(err, allFiles) {
+        var groups = [];
+        var count = 1;
+        var doneFiles = {};
+
+        console.log("Filtering out already-uploaded files...");
+
+        meData.result.forEach(function(file) {
+            doneFiles[file] = true;
+        });
+
+        // Filter out files that've already been uploaded
+        allFiles = allFiles.filter(function(file) {
+            var meFile = (args.dir ? args.dir + "/" : "") +
+                path.basename(file);
+            return !(meFile in doneFiles);
+        });
+
+        // Map the files to their real location
+        allFiles = allFiles.map(function(file) {
+            return path.resolve(args.path, file);
+        });
+
+        // Group the files into batches of files to upload
+        for (var i = 0; i < allFiles.length; i += args.batch) {
+            groups.push(allFiles.slice(i, i + args.batch));
+        }
+
+        async.eachSeries(groups, function(files, callback) {
+            console.log("Uploading batch [" +
+                count + "/" + groups.length + "]");
+
+            ME.add(files, args.dir, function(err) {
+                console.log("Batch done #" + count);
+                count += 1;
+
+                // Pause at the end of the upload
+                setTimeout(callback, args.pause);
+            });
+        }, function(err) {
+            console.log("DONE");
+        });
+    });
+});
---
+++ get-similar.js
@@ -0,0 +1,80 @@
+var fs = require("fs");
+var async = require("async");
+var JSONStream = require("JSONStream");
+var ArgumentParser = require("argparse").ArgumentParser;
+
+var argparser = new ArgumentParser({
+    description: "Downloads image similarity data from MatchEngine. " +
+        "Outputs JSON results to the specified file."
+});
+
+argparser.addArgument(["outFile"], {
+    help: "File to which the JSON output should be written."
+});
+
+argparser.addArgument(["--conf"], {
+    defaultValue: __dirname + "/me.conf.json",
+    help: "The JSON config file containing MatchEngine auth details " +
+        "(default: me.conf.json)."
+});
+
+argparser.addArgument(["--threads"], {
+    defaultValue: 2,
+    type: "int",
+    help: "Number of simulataneous requests to make (default: 2, max: 4)."
+});
+
+argparser.addArgument(["--filter"], {
+    help: "Filter for which MatchEngine files to download the results of."
+});
+
+var args = argparser.parseArgs();
+
+var conf = require(args.conf);
+var ME = require("matchengine")(conf);
+
+var numDownloaded = 0;
+var totalImages = 0;
+var fileStream;
+
+console.log("Loading Match Engine data...");
+
+ME.list(function(data) {
+    fileStream = JSONStream.stringifyObject();
+    fileStream.pipe(fs.createWriteStream(args.outFile));
+
+    var results = data.result;
+
+    if (args.filter) {
+        results = results.filter(function(image) {
+            return image.indexOf(args.filter) === 0;
+        });
+    }
+
+    totalImages = results.length;
+
+    async.eachLimit(results, args.threads, queryImage, function(err) {
+        console.log("Done Querying Match Engine.");
+        fileStream.end();
+    });
+});
+
+function queryImage(image, callback) {
+    numDownloaded += 1;
+    console.log("[" + numDownloaded + "/" + totalImages +
+        "] Querying " + image + "...");
+
+    ME.similar(image, function(data) {
+        // Filter out results that are just matching the same image
+        var results = data.result.filter(function(item) {
+            return item.filepath !== image;
+        });
+
+        // Only log results where there is at least one match
+        if (results.length > 0) {
+            fileStream.write([image, results]);
+        }
+
+        callback();
+    });
+}
